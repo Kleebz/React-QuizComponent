165c11b2eb9e638e2eaa2320cb9ae6e4f99a1ae9
Jacob <jacobmorosoff@gmail.com>
2020-06-29 22:43:16 -0700

    this one is mostly right, loop not printing components properly

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -4,16 +4,26 @@ import QuizQuestionButton from './QuizQuestionButton.js'
 class QuizQuestion extends Component{
     
     render(){
+      var index = 0;
+
         return <main>
         <section>
           <p>{this.props.quiz_question.instruction_text}</p>
         </section>
         <section className="buttons">
           <ul>
-              <QuizQuestionButton button_text={this.props.quiz_question.answer_options[0]}></QuizQuestionButton>
+            {this.props.quiz_question.answer_options.forEach(answer_option => {
+              index++;
+              console.log(answer_option + ' ' + index );
+              <QuizQuestionButton key={index} button_text={index + '.' + answer_option} clickHandler={this.handleClick.bind(this)}></QuizQuestionButton>;})}
+              {/* <QuizQuestionButton button_text={this.props.quiz_question.answer_options[0]}></QuizQuestionButton> */}
           </ul>
         </section>
       </main>
+    }
+
+    handleClick(buttonText){
+      buttonText == this.props.quiz_question.answer ? this.props.showNextQuestionHandler() : console.log('still going');
     }
 }
 
