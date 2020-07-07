8d5197d82fc47165f23d8bd876dd8acd8ce90222
Jacob <jacobmorosoff@gmail.com>
2020-07-06 21:57:53 -0700

    done?

--- src/QuizEnd.js
+++ src/QuizEnd.js
@@ -3,12 +3,16 @@
 import React, { Component } from 'react'
 
 class QuizEnd extends Component{
-
+  //Jacob ToDo: Remember, This syntax will be used a lot. Remember how to bind!
     render(){
         return <div>
         <p>Thanks for playing!</p>
-        <a href=''>Reset Quiz</a>
+        <a href='' onClick={this.handleResetClick.bind(this)}>Reset Quiz</a>
       </div>
+    }
+
+    handleResetClick(){
+      this.props.resetClickHandler();
     }
 }
 
